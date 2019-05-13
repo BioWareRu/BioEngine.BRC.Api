b7fc1569c1601a4bfba2b335bfffb78cdddbb72c
George <sonicgd@gmail.com>
2019-05-13 15:10:26 +0500

    Use new modules

--- src/BioEngine.BRC.API/Program.cs
+++ src/BioEngine.BRC.API/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Text;
+using BioEngine.BRC.Domain;
 using BioEngine.BRC.Domain.Entities;
-using BioEngine.Core;
 using BioEngine.Core.API;
-using BioEngine.Core.Infra;
+using BioEngine.Core.DB;
+using BioEngine.Core.Logging.Loki;
+using BioEngine.Core.Search.ElasticSearch;
+using BioEngine.Core.Seo;
+using BioEngine.Core.Storage;
 using BioEngine.Extra.Ads;
-using BioEngine.Extra.Ads.Entities;
 using BioEngine.Extra.Facebook;
 using BioEngine.Extra.IPB;
 using BioEngine.Extra.Twitter;
@@ -25,26 +28,59 @@ namespace BioEngine.BRC.Api
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .AddBioEngineModule<CoreModule, CoreModuleConfig>(config =>
+            new Core.BioEngine(args)
+                .AddModule<PostgresDatabaseModule, PostgresDatabaseModuleConfig>(
+                    (config, configuration, env) =>
+                    {
+                        config.Host = configuration["BE_POSTGRES_HOST"];
+                        config.Port = int.Parse(configuration["BE_POSTGRES_PORT"]);
+                        config.Username = configuration["BE_POSTGRES_USERNAME"];
+                        config.Password = configuration["BE_POSTGRES_PASSWORD"];
+                        config.Database = configuration["BE_POSTGRES_DATABASE"];
+                        config.EnablePooling = env.IsDevelopment();
+                        config.MigrationsAssembly = typeof(Developer).Assembly;
+                    })
+                .AddModule<BrcDomainModule>()
+                .AddModule<ElasticSearchModule, ElasticSearchModuleConfig>((config, configuration, env) =>
                 {
-                    config.Assemblies.Add(typeof(Developer).Assembly);
-                    config.Assemblies.Add(typeof(Ad).Assembly);
-                    config.EnableValidation = true;
-                    config.MigrationsAssembly = typeof(Developer).Assembly;
-                    config.EnableElasticSearch = true;
+                    config.Url = configuration["BE_ELASTICSEARCH_URI"];
+                    config.Login = configuration["BE_ELASTICSEARCH_LOGIN"];
+                    config.Password = configuration["BE_ELASTICSEARCH_PASSWORD"];
                 })
-                .AddBioEngineModule<ApiModule, ApiModuleConfig>(config =>
+                .AddModule<ApiModule>()
+                .AddModule<LokiLoggingModule, LokiLoggingConfig>((config, configuration, env) =>
                 {
-                    config.Assemblies.Add(typeof(Startup).Assembly);
-                    config.Assemblies.Add(typeof(Extra.Ads.Api.Entities.Ad).Assembly);
+                    config.Url = configuration["BRC_LOKI_URL"];
                 })
-                .AddBioEngineModule<InfraModule>()
-                .AddBioEngineModule<IPBApiModule>()
-                .AddBioEngineModule<IPBAuthModule>()
-                .AddBioEngineModule<TwitterModule>()
-                .AddBioEngineModule<FacebookModule>()
-                .AddBioEngineModule<AdsModule>()
+                .AddModule<S3StorageModule, S3StorageModuleConfig>((config, configuration, env) =>
+                {
+                    var uri = configuration["BE_STORAGE_PUBLIC_URI"];
+                    var success = Uri.TryCreate(uri, UriKind.Absolute, out var publicUri);
+                    if (!success)
+                    {
+                        throw new ArgumentException($"URI {uri} is not proper URI");
+                    }
+
+                    var serverUriStr = configuration["BE_STORAGE_S3_SERVER_URI"];
+                    success = Uri.TryCreate(serverUriStr, UriKind.Absolute, out var serverUri);
+                    if (!success)
+                    {
+                        throw new ArgumentException($"S3 server URI {uri} is not proper URI");
+                    }
+
+                    config.PublicUri = publicUri;
+                    config.ServerUri = serverUri;
+                    config.Bucket = configuration["BE_STORAGE_S3_BUCKET"];
+                    config.AccessKey = configuration["BE_STORAGE_S3_ACCESS_KEY"];
+                    config.SecretKey = configuration["BE_STORAGE_S3_SECRET_KEY"];
+                })
+                .AddModule<SeoModule>()
+                .AddModule<IPBApiModule>()
+                .AddModule<IPBAuthModule>()
+                .AddModule<TwitterModule>()
+                .AddModule<FacebookModule>()
+                .AddModule<AdsModule>()
+                .GetHostBuilder()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
