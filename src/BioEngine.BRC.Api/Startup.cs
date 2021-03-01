007122a5a8ebd788d254c2e9e1d1962b265dc0a0
George Drak <sonicgd@gmail.com>
2021-03-01 18:35:01 +0500

    Upd BioEngine.BRC.*, drop elastic apm

--- src/BioEngine.BRC.Api/Startup.cs
+++ src/BioEngine.BRC.Api/Startup.cs
@@ -5,7 +5,6 @@ using BioEngine.BRC.Common;
 using BioEngine.Core.Api;
 using BioEngine.Core.Web;
 using BioEngine.Extra.IPB.Controllers;
-using Elastic.Apm.NetCoreAll;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Routing;
@@ -77,15 +76,6 @@ namespace BioEngine.BRC.Api
         {
             endpoints.AddBrcRoutes();
             base.ConfigureEndpoints(app, env, endpoints);
-        }
-
-        protected override void ConfigureStart(IApplicationBuilder appBuilder)
-        {
-            base.ConfigureStart(appBuilder);
-            if (Environment.IsProduction())
-            {
-                appBuilder.UseAllElasticApm(Configuration);
-            }
         }
     }
 
